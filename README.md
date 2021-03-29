92d2d6a44d9ed178ac62e8c47625fb60c2b416a7
Mike <tankf33der@disroot.org>
2021-03-29 14:08:48 +0300

    .

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Check HACL* C crypto code against Monocypher and TIS-CI.
 
 - Latest monocypher
-- HACL* v0.3.0
+- HACL* v0.3.0 (c89-compatible)
+
 
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "monocypher.h"
+#include "Hacl_Poly1305_32.h"
+
+
+int main(void) {
+	return 0;
+}
