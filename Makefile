459c42a68206cafec96ad08658145356b2e3edad
Mike <tankf33der@disroot.org>
2021-03-29 14:48:21 +0300

    .

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 all:
-	clang -fsanitize=address -I. -I kremlib/dist/minimal main.c Hacl_Poly1305_32.c monocypher.c && ./a.out
-	clang -fsanitize=memory -I. -I kremlib/dist/minimal main.c Hacl_Poly1305_32.c monocypher.c && ./a.out
-	clang -fsanitize=undefined -I. -I kremlib/dist/minimal main.c Hacl_Poly1305_32.c monocypher.c && ./a.out
+	gcc -I. -I kremlib/dist/minimal main.c Hacl_Poly1305_32.c monocypher.c && ./a.out
 clean:
 	rm -rf *.o *.out
--- monocypher.c
+++ monocypher.c
@@ -85,14 +85,14 @@ static size_t align(size_t x, size_t pow_2)
     return (~x + 1) & (pow_2 - 1);
 }
 
-static u32 load24_le(const u8 s[3])
+u32 load24_le(const u8 s[3])
 {
     return (u32)s[0]
         | ((u32)s[1] <<  8)
         | ((u32)s[2] << 16);
 }
 
-static u32 load32_le(const u8 s[4])
+u32 load32_le(const u8 s[4])
 {
     return (u32)s[0]
         | ((u32)s[1] <<  8)
@@ -100,12 +100,12 @@ static u32 load32_le(const u8 s[4])
         | ((u32)s[3] << 24);
 }
 
-static u64 load64_le(const u8 s[8])
+u64 load64_le(const u8 s[8])
 {
     return load32_le(s) | ((u64)load32_le(s+4) << 32);
 }
 
-static void store32_le(u8 out[4], u32 in)
+void store32_le(u8 out[4], u32 in)
 {
     out[0] =  in        & 0xff;
     out[1] = (in >>  8) & 0xff;
@@ -113,22 +113,22 @@ static void store32_le(u8 out[4], u32 in)
     out[3] = (in >> 24) & 0xff;
 }
 
-static void store64_le(u8 out[8], u64 in)
+void store64_le(u8 out[8], u64 in)
 {
     store32_le(out    , (u32)in );
     store32_le(out + 4, in >> 32);
 }
 
-static void load32_le_buf (u32 *dst, const u8 *src, size_t size) {
+void load32_le_buf (u32 *dst, const u8 *src, size_t size) {
     FOR(i, 0, size) { dst[i] = load32_le(src + i*4); }
 }
-static void load64_le_buf (u64 *dst, const u8 *src, size_t size) {
+void load64_le_buf (u64 *dst, const u8 *src, size_t size) {
     FOR(i, 0, size) { dst[i] = load64_le(src + i*8); }
 }
-static void store32_le_buf(u8 *dst, const u32 *src, size_t size) {
+void store32_le_buf(u8 *dst, const u32 *src, size_t size) {
     FOR(i, 0, size) { store32_le(dst + i*4, src[i]); }
 }
-static void store64_le_buf(u8 *dst, const u64 *src, size_t size) {
+void store64_le_buf(u8 *dst, const u64 *src, size_t size) {
     FOR(i, 0, size) { store64_le(dst + i*8, src[i]); }
 }
 
