5ed70417c72802a51072edc179a0f233f850053e
Mike <tankf33der@disroot.org>
2021-03-30 09:15:29 +0300

    .

--- main.c
+++ main.c
@@ -70,21 +70,19 @@ Hacl_Blake2b_32_blake2b(
 -1, 1 8 16
 */
 
-/*
     for(size_t h = 1; h < 64; h += 8)
         for(size_t k = 8; k < 64; k += 8)
             for(size_t i = 8; i < 64; i += 8) {
             	hash1[0] = 123;
                 crypto_blake2b_general(hash1, h, key, k, in, i);
                 Hacl_Blake2b_32_blake2b(h, hash2, i, in, k, key);
-                status |= crypto_verify64(hash1, hash2);
-                printf("%d, %d %d %d\n", status, h, k, i);
+                // status |= crypto_verify64(hash1, hash2);
+                // printf("%d, %d %d %d\n", status, h, k, i);
             }
-*/
 
-	hash1[0] = 123;
-    crypto_blake2b_general(hash1, 1, key, 8, in, 0);
-    Hacl_Blake2b_32_blake2b(1, hash2, 0, in, 8, key);
+	// hash1[0] = 123;
+    // crypto_blake2b_general(hash1, 1, key, 8, in, 0);
+    // Hacl_Blake2b_32_blake2b(1, hash2, 0, in, 8, key);
     //status |= crypto_verify64(hash1, hash2);
                         
     return status;
