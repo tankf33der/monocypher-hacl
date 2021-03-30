2d7c870921954ba012703897409a1276a5fadadb
Mike <tankf33der@disroot.org>
2021-03-30 06:31:19 +0300

    .

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ static int p1305(void) {
     ARRAY(in,  64);
     int status = 0;
     for(size_t i = 0; i < 64; i++) {
+        mac1[i] = 123;
         crypto_poly1305(mac1, in, i, key);
         Hacl_Poly1305_32_poly1305_mac(mac2, i, in, key);
         status |= crypto_verify16(mac1, mac2);
@@ -31,7 +32,9 @@ int x25519(void) {
     ARRAY(pub1, 32);
     ARRAY(pub2, 32);
     key[0] = 0;
+	pub1[0] = 123;
     int status = 0;
+    
     crypto_x25519_public_key(pub1, key);
     Hacl_Curve25519_51_secret_to_public(pub2, key);
     status |= crypto_verify32(pub1, pub2);
